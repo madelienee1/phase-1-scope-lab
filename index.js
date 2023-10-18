1f20fe19ae315bceb4c11be4ae0abc24650363da
Madeliene <madelienestiller@gmail.com>
2023-10-18 20:06:31 +1000

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+// Bob
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+console.log(customerName);
+
+// Not Bob
+
+function setBestCustomer() {
+  window.bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "Bob";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Winnie";
+}
